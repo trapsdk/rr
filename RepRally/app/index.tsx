a89cb51da59a32d1ad6ca5545efc25aaabedaa07
gab3 <gabrielreuben05@gmail.com>
2024-11-05 19:34:50 -0800

    Welcome Screen almost done

--- RepRally/app/index.tsx
+++ RepRally/app/index.tsx
@@ -1,48 +1,94 @@
 import React from 'react';
-import {View, Text, ImageBackground, Image, StyleSheet, ImageComponent} from 'react-native';
+import {
+    View,
+    Text,
+    ImageBackground,
+    Image,
+    StyleSheet,
+    ImageComponent,
+    SafeAreaView,
+    TouchableOpacity
+} from 'react-native';
 
 
 
 const imgLogo = <Image source= {require('./img/rrlogo.png')}/>
 
-export default function (){
+export default function Welcome(){
     return (
 
-        <View style={[
-            styles.container,
-            {
-                flexDirection: 'column',
-            },
-        ]}>
-            <View style={{flex:.2}}>
-            <Image source={require('./img/rrlogo.png')} />
+        <ImageBackground source = {require('./img/bg.png')} style={styles.bg}>
+
+        <SafeAreaView>
+            <View style = {styles.container}>
+                <Image style={styles.image} source={require('./img/rrlogo.png')}/>
+            </View>
+            <View style={styles.textcenter}>
+                <Text style={styles.text}>Welcome! </Text>
+            </View>
+            <View style={styles.containerThree}>
+                <TouchableOpacity style={styles.button}>
+                    <Text style={styles.buttontext}>Get Started</Text>
+                </TouchableOpacity>
             </View>
 
 
-
-
-
-        </View>
-
+        </SafeAreaView>
+        </ImageBackground>
     );
-
 }
 
-// <ImageBackground source = {require('./img/bg.png')} style={bgstyle.container}>
-//
-// </ImageBackground>
+const styles = StyleSheet.create({
+    button:{
+        backgroundColor: 'black',
+        borderRadius: 50,
+        paddingHorizontal: 100,
+        paddingVertical: 25,
+        bottom: -100,
+    },
+    buttontext:{
+        fontSize: 30,
+        fontFamily: "Poppins-Regular.ttf",
+        width: '100%',
+        color: 'white',
+        justifyContent: 'center',
+        alignItems: 'center',
 
-
-const bgstyle = StyleSheet.create({
+    },
     container:{
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    image: {
+        top: 50,
+        width: '100%',
+        height: '100%',
+        resizeMode: 'contain',
+    },
+
+    textcenter: {
+        flex: 2,
+        justifyContent: 'center', // Center vertically
+        alignItems: 'center', // Center horizontally
+    },
+    containerThree:{
+        flex: 3,
+        paddingHorizontal: 20,
+        justifyContent: 'center', // Center vertically
+        alignItems: 'center', // Center horizontally
+    },
+    text: {
+        top: -50,
+        color: 'black',
+        fontWeight: 'bold',
+        fontSize: 25,
+
+    },
+    bg:{
         width: '100%',
         height: '100%',
         justifyContent: 'center',
         alignItems: 'center',
-    }
-})
-const styles = StyleSheet.create({
-    container:{
-        flex: 1,
-    }
-})
+    },
+});
